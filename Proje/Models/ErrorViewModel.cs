0202b7ea6ff84f74fa000f720ec1b837161e5fed
Resul Çalışkan <resulcaliskansau@gmail.com>
2022-12-19 14:14:20 +0300

    ProjeLastCommit

---
+++ Proje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OgrTum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Proje/Models/Ogrenci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OgrTum.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name = "Öğrenci No")]
+        public string OgrNo { get; set; }
+        [Required(ErrorMessage = "Ad alanı zorunlu")]
+        [Display(Name = "Öğrenci Ad")]
+        public string OgrAd { get; set; }
+        [Display(Name = "Öğrenci Soyad")]
+        public string OgrSoyad { get; set; }
+    }
+   
+}
