ac4f0a58210bd3f8dad6c1c2df0737f95c2f7e9c
Resul Çalışkan <resulcaliskansau@gmail.com>
2022-12-20 19:12:07 +0300

    login pages

---
+++ Web_Projem/Web_Projem/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Projem.Models
+{
+    public class Users
+    {
+        [Display(Name="Kullanıcı Adı")]
+        [Required]
+        public String UserName { get; set; }
+        [Required]
+        [Display(Name ="Şifre")]
+        public String Password { get; set; }
+    }
+}
