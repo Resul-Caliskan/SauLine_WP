41cfa02ecf223443b77b49f673f74ddc321bcabb
Resul Çalışkan <resulcaliskansau@gmail.com>
2022-12-23 20:57:33 +0300

    register

--- Web_Projem/Web_Projem/Controllers/LoginController.cs
+++ Web_Projem/Web_Projem/Controllers/LoginController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web_Projem.Controllers
 {
     public class LoginController : Controller
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "gcXSL3OsYxwnTQ4l2MlbJ2O1LN3k8gD1ZLbSO0Pp",
+            BasePath = "https://crsmartled-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
         public IActionResult Index()
         {
             return View();
