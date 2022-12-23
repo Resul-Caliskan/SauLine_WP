8a136b156579a34e758b5fd4c74deacc80c5230e
Resul Çalışkan <resulcaliskansau@gmail.com>
2022-12-23 23:08:54 +0300

    firebase crud1

--- Web_Projem/Web_Projem/Controllers/LoginController.cs
+++ Web_Projem/Web_Projem/Controllers/LoginController.cs
@@ -1,6 +1,12 @@
-﻿using FireSharp.Config;
+﻿using FireSharp;
+using FireSharp.Config;
 using FireSharp.Interfaces;
+using FireSharp.Response;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Protocol;
+using System.Text.Json;
+using Web_Projem.Models;
 
 namespace Web_Projem.Controllers
 {
@@ -11,9 +17,50 @@ namespace Web_Projem.Controllers
             AuthSecret = "gcXSL3OsYxwnTQ4l2MlbJ2O1LN3k8gD1ZLbSO0Pp",
             BasePath = "https://crsmartled-default-rtdb.europe-west1.firebasedatabase.app/"
         };
-        public IActionResult Index()
+        FirebaseClient client;
+        public IActionResult Index() 
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Users user) 
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Users");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Users>();
+            Boolean dogru;
+            foreach (var item in data)
+            {
+                list.Add(JsonConvert.DeserializeObject<Users>(((JsonProperty)item).Value.ToString()));
+               
+            }
+            return View(list);
+        }
+        [HttpPost]
+        public IActionResult Register(Users user) {
+            try
+            {
+                AddUser(user);
+                ModelState.AddModelError(String.Empty,"Added Successfuly");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(String.Empty, ex.Message);
+                return View();
+            }
+
+            
+        }
+        private void AddUser(Users user)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            var data = user;
+            PushResponse respone = client.Push("Users/",data);
+            //data.UserName = respone.Result.name;
+            //SetResponse setResponse = client.Set("Users/"+data.UserName,data);
         }
     }
 }
--- Web_Projem/Web_Projem/Models/Users.cs
+++ Web_Projem/Web_Projem/Models/Users.cs
@@ -4,11 +4,11 @@ namespace Web_Projem.Models
 {
     public class Users
     {
-        [Display(Name="Kullanıcı Adı")]
-        [Required(ErrorMessage ="Kullanıcı Adı Alanı Zorunlu")]
-        public String? UserName { get; set; }
-        [Required(ErrorMessage ="Şifre Alanı Zorunlu")]
-        [Display(Name ="Şifre")]
-        public String? Password { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı Adı Alanı Zorunlu")]
+        public String UserName { get; set; }
+        [Required(ErrorMessage = "Şifre Alanı Zorunlu")]
+        [Display(Name = "Şifre")]
+        public String Password { get; set; }
     }
 }
